fd5b332b7f1ff57d042f04041714d8f4873e6f14
Naseef Sami <cmnsami@gmail.com>
2020-04-02 15:25:44 +0000

    Automatically backed up by Learn

---
+++ example.rb
@@ -0,0 +1,13 @@
+
+puts "Hello World\n"
+Hello World
+
+ 
+print "Hello World\n"
+Hello World
+
+ 
+p "Hello World\n"
+"Hello World"
+
+ 
---
+++ greeting.rb
@@ -0,0 +1,22 @@
+
+def greeting_programmer(name, language)
+puts "Hello World\n"
+Hello World
+ => nil
+ 
+print "Hello World\n"
+Hello World
+ => nil
+ 
+p "Hello World\n"
+"Hello World"
+ => "Hello World\n"
+
+end
+ 
+greeting_programmer("Maria", "Ruby")
+# > Hello, Maria. We heard you are a great Ruby programmer.
+ 
+greeting_programmer("Steven", "Elixir")
+# > Hello, Steven. We heard you are a great Elixir programmer.
+
